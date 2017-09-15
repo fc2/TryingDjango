a1c8795926ceada85cb4d97fe0de5d4d9d15193b
fanny <fc2@cin.ufpe.br>
2017-09-15 01:02:21 -0300

    adding base project

---
+++ intro.py
@@ -0,0 +1,12 @@
+#comments
+
+#definindo uma function
+def hi(name):
+    print("Hi "+name+"!")
+
+counter = 1
+girls = ["belly","paty","luna","fabi"]
+for name in girls:
+    print(counter)
+    counter+1
+    hi(name)
