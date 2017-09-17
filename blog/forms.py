a4d60c562bcd46d21abcb051dc138823a3d2ebad
fanny <fc2@cin.ufpe.br>
2017-09-17 00:39:29 -0300

    Creating a form for the Post model

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+
+#tell Django which model should be used to create the form 
+    class Meta:
+        model = Post
+        fields = ('title','text',)
