60d1586368caf6b4cbdc7c567befefb54779442d
fanny <fc2@cin.ufpe.br>
2017-09-17 17:58:23 -0300

    Saving a image through the form

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
 
 
-#tell Django which model should be used to create the form 
+#tell Django which model should be used to create the form
     class Meta:
         model = Post
-        fields = ('title','text',)
+        fields = ('title','text','image')
