d0eed7a2bb9104c12393e929a88c3b8415e4d1d7
fanny <fc2@cin.ufpe.br>
2017-09-17 00:16:18 -0300

    Adding a post's detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -10,3 +10,7 @@ def post_list(request):
     #using QuerySet to get the posts from the database
     posts = Post.objects.order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html',{'post':post})
